e72a5bd654bc47720774515f98fa7f3e192d58fb
Mushtaq Ahmed <mushtaq.a@gmail.com>
2017-08-18 10:51:53 +0530

    day3b

--- src/main/scala/example/JsDemo.scala
+++ src/main/scala/example/JsDemo.scala
@@ -4,15 +4,22 @@ import play.api.libs.json._
 
 object JsDemo {
   def transform(json: JsValue): JsValue = json match {
-    case JsNumber(value)                             ⇒ JsNumber(value * value)
-    case JsArray(values)                             ⇒ JsArray(values.map(transform))
-    case JsObject(values)                            ⇒ JsObject(
+    case JsNumber(value)           ⇒ JsNumber(value * value)
+    case JsArray(values)           ⇒ JsArray(values.map(transform))
+    case JsObject(values)          ⇒ JsObject(
       values.map {
         case (k@("m" | "n"), v) ⇒ (k, v)
         case (k, v)             ⇒ (k, transform(v))
       }
     )
-    case JsString(value) if value.startsWith("urn:") ⇒ JsString("hello " + value.stripPrefix("urn:"))
-    case _                                           ⇒ json
+    case JsString("urn" :: suffix) ⇒ JsString(s"Hello $suffix")
+    case _                         ⇒ json
+  }
+}
+
+object :: {
+  def unapply(s: String): Option[(String, String)] = s.split(":") match {
+    case Array(prefix, suffix) ⇒ Some((prefix, suffix))
+    case _                     ⇒ None
   }
 }
