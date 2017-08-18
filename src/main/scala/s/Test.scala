b5a66656e41028410a43ec5abf6bbed2bcea7c29
Mushtaq Ahmed <mushtaq.a@gmail.com>
2017-08-18 13:01:46 +0530

    day3e

--- src/main/scala/s/Test.scala
+++ src/main/scala/s/Test.scala
@@ -4,6 +4,9 @@ object Test extends App {
 
   println(S.sort(List(11, 3, 56, 8))(Ord.intOrd))
   println(S.sort(List("z", "a", "m"))(Ord.strOrd))
-  println(S.sort(List(Some(11), None, Some(3)))(Ord.intOrd))
+  println(S.sort(List(Some(11), None, Some(3)))(Ord.optOrd(Ord.intOrd)))
+  println(S.sort(List(Some("z"), None, Some("a")))(Ord.optOrd(Ord.strOrd)))
+  println(S.sort(List((54, "z"), (10, "z"), (10, "m")))(Ord.pairOrd(Ord.intOrd, Ord.strOrd)))
+  println(S.sort(List((Some(54), Some("z")), (Some(10), Some("z")), (Some(10), None)))())
 
 }
